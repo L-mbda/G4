0c75deeeb62af41f44b68e50e32b40966e553c42
Defy <61808223+de-y@users.noreply.github.com>
2024-08-06 17:29:51 +0000

    Finished account creation process

--- library/functions.py
+++ library/functions.py
@@ -1,7 +1,67 @@
-import sys, os
+# Modules
+from colorama import Fore, init
+from pathlib import Path
+import sys, os, hashlib
 
+# Functions
 def clear():
   if (sys.platform == 'linux' or sys.platform == 'darwin'):
     os.system("clear")
   elif sys.platform == 'win32':
     os.system("cls")
+
+def createAccount(username, password, user_type):
+  user_dir_check = Path('user').is_dir()
+  if (not user_dir_check):
+    os.makedirs('user')
+  else:
+    pass
+  os.chdir((os.getcwd() + '/user'))
+  if user_type == 'owner':
+    if (not Path(username).is_dir()):
+      os.makedirs(username)
+    masterUser = open('privileges.g4', 'w')
+    masterUser.write(f'{username}\n')
+    masterUser.close()
+    os.chdir(f'./{username}')
+    passwordWriter = open('credentials.g4','w')
+    sha = hashlib.new('sha3_512')
+    sha.update(password.encode())
+    passwordWriter.write(sha.hexdigest())
+    passwordWriter.close()
+    roleSetter = open('account.g4', 'w')
+    roleSetter.write('owner'.encode().hex())
+    roleSetter.close()
+    os.chdir(os.getcwd())
+    print(f"\n{Fore.BLUE}Created owner account successfully.")
+  elif user_type == 'admin':
+    if (not Path(username).is_dir()):
+      os.makedirs(username)
+    privileges = open('privileges.g4', 'a')
+    privileges.write(f'{username}\n')
+    privileges.close()
+    os.chdir(f'./{username}')
+    passwordWriter = open('credentials.g4','w')
+    sha = hashlib.new('sha3_512')
+    sha.update(password.encode())
+    passwordWriter.write(sha.hexdigest())
+    passwordWriter.close()
+    roleSetter = open('account.g4', 'w')
+    roleSetter.write('admin'.encode().hex())
+    roleSetter.close()
+    os.chdir(os.getcwd())
+    print(f"\n{Fore.BLUE}Created administrator account successfully.")
+  else:
+    if (not Path(username).is_dir()):
+      os.makedirs(username)
+    os.chdir(f'./{username}')
+    passwordWriter = open('credentials.g4','w')
+    sha = hashlib.new('sha3_512')
+    sha.update(password.encode())
+    roleSetter = open('account.g4', 'w')
+    roleSetter.write('user'.encode().hex())
+    roleSetter.close()
+    os.chdir(os.getcwd())
+    passwordWriter.write(sha.hexdigest())
+    passwordWriter.close()
+    print(f"\n{Fore.BLUE}Created account successfully.")
--- platform.py
+++ platform.py
@@ -17,17 +17,27 @@ setup = False
 library.functions.clear()
 init()
 
-setup_check = Path('user').is_dir()
+# setup_check = Path('user').is_dir()
+setup_check = False
+
 if (not setup_check):
   print(f"""{Fore.GREEN}Welcome to {Fore.CYAN}G4{Fore.RESET}.
 
   Let's create your account.
   """)
+  credentials = []
   while True:
     username = input(f"{Fore.MAGENTA} Username{Fore.RESET}: ")
     password = pwinput.pwinput(f"{Fore.YELLOW} Password{Fore.RESET}: ")
     confirm_password = pwinput.pwinput(f"{Fore.YELLOW} Confirm Password{Fore.RESET}: ")
-    if password == confirm_password:
+    if username == '' or username == ' ':
+      print(f"\n{Fore.GREEN}You haven\'t inputted a username, please input one before continuing.'")
+    elif password == confirm_password and (username != '' or username != ' '):
+      credentials.append([username, password])
       break
     else:
       print(f"\n{Fore.GREEN}The password isn\'t confirmed. Please reconfirm the password before continuing.")
+  # Setup process continues here
+  print(f"\n{Fore.LIGHTYELLOW_EX}Confirmed credentials, please wait while your account is created.{Fore.RESET}")
+  library.functions.createAccount(credentials[0][0], credentials[0][1], 'owner')
+  
